19720614679cb6e77017f9ee7cb3a8f81e51bf17
Emmanuel Tran <emmanuel.tran@datadoghq.com>
2021-11-10 16:45:01 +0100

    Update Karabiner setup

--- Scripts/focusZoom.sh
+++ Scripts/focusZoom.sh
@@ -1,6 +1,5 @@
-WINID=$(yabai -m query --windows | jq '((.[]| select(.app == "zoom.us")).id)');
+WINID=$(yabai -m query --windows | jq '((.[]| select((.app == "zoom.us") and (.title == "Zoom Meeting"))).id)');
 if [ -z "$WINID" ]; then
-    /Applications/zoom.us.app/Contents/MacOS/zoom.us
     skhd --key "alt - 0x15"
 else
     yabai -m window --focus "$WINID"
